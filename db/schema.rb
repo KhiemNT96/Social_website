b8486995cc75c1d112d8ce456baa9369a88cc085
KhiemNT96 <14110257@student.hcmute.edu.vn>
2017-12-22 17:24:02 +0700

    Added post form to components sub directory

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171219103347) do
+ActiveRecord::Schema.define(version: 20171222070513) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.text "content"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id", "created_at"], name: "index_posts_on_user_id_and_created_at"
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -29,9 +38,10 @@ ActiveRecord::Schema.define(version: 20171219103347) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "username"
-  end
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
+  end
 
+  add_foreign_key "posts", "users"
 end
